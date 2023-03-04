f3abf610d89354e74a808ffa6434f6bc71056580
Purityndirangu <ndiranguwanjikupurity@gmail.com>
2023-03-04 21:56:46 +0300

    Wrote variables and their functions

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,140 @@
+fun main(){
+fullName("Joe","Mamva")
+    ages(45)
+    phoneNumber(+245234567)
+    weightInKg(56)
+    nationality("Kenyan")
+
+}
+fun fullName (firstName: String,lastName: String){
+    var names = firstName + lastName
+    println(names)
+}
+fun ages(age: Int){
+    var result = age
+    println(result)
+}
+fun phoneNumber(num1:Int){
+    var number = num1
+    println(number)
+}
+fun weightInKg(num2:Int){
+    var weight = num2
+    println(weight)
+}
+fun nationality(citizen:String){
+    var x  = citizen
+        if (x=="Kenyan") {
+       print ("Kenyan")
+    }else{
+       print ("Not Kenyan")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
